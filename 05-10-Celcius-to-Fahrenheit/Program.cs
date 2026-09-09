680532f1d0f9505e6a87b2ac16e2727b7743653a
Valton Morina <Valton.morina301@hotmail.com>
2026-09-10 00:13:37 +0200

    Exercise 05-10 Celsius to Fahrenheit

---
+++ 05-10-Celcius-to-Fahrenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿double celsius = 37.5;
+double fahrenheit = 32 + (double) 9/5 * celsius;
+
+Console.WriteLine("");
+Console.Write("The temperature is: ");
+Console.Write(fahrenheit);
+Console.WriteLine(" fahrenheit!");
+Console.WriteLine("");
+
+// It would have been enough to write 9.0/5 instead of (double),
+// since the type is already declared as a double to start with
+// My version worked because:
+// "That means the cast applies to 9 alone, not to 9/5.
+// You get (double)9 / 5, which is double / int"
