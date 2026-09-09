660dcc9be8c0a1db2ec930317aee71b2894de12f
Valton Morina <Valton.morina301@hotmail.com>
2026-09-09 02:48:53 +0200

    Forgot a semicolon

--- hello/Program.cs
+++ hello/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("If you can see this, then the command worked!")
+Console.WriteLine("If you can see this, then the command worked!");
