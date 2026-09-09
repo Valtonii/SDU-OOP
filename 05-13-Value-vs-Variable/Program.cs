adf1a72002e4e793a9b87a9cd22c1d387cb70b3f
Valton Morina <Valton.morina301@hotmail.com>
2026-09-09 21:49:32 +0200

    exercise 5.13 added

---
+++ 05-13-Value-vs-Variable/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+A value is the data. A variable is a named box you keep data in.
+
+Then three facts:
+
+A value can stand alone. Console.WriteLine(5); has a value and no box.
+A box is never empty. Declare one without assigning and C# puts a zero in it.
+The box's type decides what you're allowed to put in it.
+
+And the bit worth remembering above all: boxes change, values don't.
+x = 7 swaps what's in the box. It doesn't turn the number 5 into 7.
+*/
