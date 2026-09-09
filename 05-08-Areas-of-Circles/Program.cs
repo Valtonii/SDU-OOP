acf663934979c2af00a5f8a6bc3192e3b358adf9
Valton Morina <Valton.morina301@hotmail.com>
2026-09-09 23:33:39 +0200

    Exercise 05.08 added, Areas of Circles

---
+++ 05-08-Areas-of-Circles/Program.cs
@@ -0,0 +1,16 @@
+﻿const double pi = 3.14159;
+int radius = 1;
+
+float area = (float)(pi * radius * radius);
+
+Console.WriteLine(area);
+
+radius += 2;
+area = (float)(pi * radius * radius);
+
+Console.WriteLine(area);
+
+radius += 2;
+area = (float)(pi * radius * radius);
+
+Console.WriteLine(area);
