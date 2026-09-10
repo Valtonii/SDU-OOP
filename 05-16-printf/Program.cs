c57641f9c60255c43648464ca8ee477183020709
Valton Morina <Valton.morina301@hotmail.com>
2026-09-10 20:52:41 +0200

    Exercise 05-16-printf done

--- 05-16-printf/Program.cs
+++ 05-16-printf/Program.cs
@@ -1,4 +1,8 @@
 ﻿Console.WriteLine("Hello, World!");
 
-
-
+int i = 42;
+long l = 56;
+float f = 3.14159F;
+double d = 3.14159 * 10;
+Console.WriteLine("i = {0} \nl = {1,4} \nf = {2} \nd = {3,6:0.0000}", i, l, f, d);
+Console.WriteLine("{1} then {0}", i, l);
