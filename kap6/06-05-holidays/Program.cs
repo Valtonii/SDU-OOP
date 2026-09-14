41803a2ba2c1feddf19026bbf84b8d5c4de77a1b
Valton Morina <Valton.morina301@hotmail.com>
2026-09-14 20:26:11 +0200

    06-05-holiday exercise added

---
+++ kap6/06-05-holidays/Program.cs
@@ -0,0 +1,23 @@
+﻿int month = 10;
+
+switch (month) {
+    case 10:
+        Console.WriteLine("Autumn holiday");
+        break;
+    case 12:
+        Console.WriteLine("Christmas holiday");
+        break;
+    case 4:
+        Console.WriteLine("Spring holiday");
+        break;
+    case 7:
+        // Console.WriteLine("Summer holiday");
+        // break;
+        // DETTE KAN FAKTISK FJERNES OG CASE VILLE STADIGE VIRKE, det er kaldet "fall-through"
+    case 8:
+        Console.WriteLine("Summer holiday");
+        break;
+    default:
+        Console.WriteLine("Hard work");
+        break;
+}
