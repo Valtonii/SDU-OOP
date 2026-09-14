ebb95d76b91938891a0432caa56c675b93a21559
Valton Morina <Valton.morina301@hotmail.com>
2026-09-14 21:11:46 +0200

    06-09 "areas of circles" exercise done

---
+++ kap6/06-09-areas-of-circles/Program.cs
@@ -0,0 +1,7 @@
+﻿const double pi = 3.14159;
+
+for (int radius = 1; radius <= 5; radius += 2) {
+    double area = pi * radius * radius;
+    Console.WriteLine("The area of this circle is {0:0.00}", area);
+    Console.WriteLine();
+}
