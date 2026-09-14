4886434f69c740c39bd770e25b575d58f9ecff8c
Valton Morina <Valton.morina301@hotmail.com>
2026-09-14 17:15:48 +0200

    Skifter til pc nu

---
+++ kap6/06-03-Christmas-sale/Program.cs
@@ -0,0 +1,24 @@
+﻿int christmasDays = 360 - 4; // Calculates the amount of days until christmas from newYear
+int secDay = 86400; // Seconds in a day 
+int secNewYear = christmasDays * secDay + 43200;
+double price = 599.95;
+double rabat = price - ((price/100)*30);
+
+int secInDays = (secNewYear/secDay); // Calculates the seconds into days
+    // Console.WriteLine(secInDays);
+
+
+
+
+bool christmas = secInDays == christmasDays;
+
+if (christmas == false) {
+    Console.WriteLine("It's not Christmas, no discount for you");
+    } else {
+        Console.Write("It's Christmas! Discount for you brother.");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.Write("It's gonna be: "); 
+        Console.WriteLine(rabat);
+        Console.WriteLine("");
+    }
