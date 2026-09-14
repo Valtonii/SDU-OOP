139bc4b08b5cc049106a8ddbdd147888a00ad2ef
Valton Morina <Valton.morina301@hotmail.com>
2026-09-14 21:27:01 +0200

    06-04 "Lengths of Months" is done

---
+++ kap6/06-04-Lengths-of-Months/Program.cs
@@ -0,0 +1,26 @@
+﻿int month = 2;
+
+switch (month)
+{
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        Console.WriteLine("31 days");
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        Console.WriteLine("30 days");
+        break;
+    case 2:
+        Console.WriteLine("28 days");
+        break;
+    default:
+        Console.WriteLine("Invalid month");
+        break;
+}
