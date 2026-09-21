3242a1057821a6862136dadf6c065e492b3df8b5
Valton Morina <Valton.morina301@hotmail.com>
2026-09-21 12:31:20 +0200

    First point giving activity done

---
+++ PA1/Pointgiving-Activity-1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] taskExample = [-7,3,-2,10,-15,0,-4];
+int largestNeg = taskExample[0];
+
+foreach (int number in taskExample) {
+    if (number<largestNeg) {
+        largestNeg = number;
+        Console.WriteLine("Largest negative value: " + largestNeg);
+    }   
+}
+/* 
+I chose to understand this task by having to print out the largest negative value (im assuming that would be -15 in my array).
+*/
