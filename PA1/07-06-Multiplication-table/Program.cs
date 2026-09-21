a4b5bbba37f88ddeeb79341523299d1c74699310
Valton Morina <Valton.morina301@hotmail.com>
2026-09-21 14:08:43 +0200

    07-06-Multiplication table done

---
+++ PA1/07-06-Multiplication-table/Program.cs
@@ -0,0 +1,9 @@
+﻿int size = 12;
+int[] threesArr = new int[size];
+
+for (int i = 0; i<threesArr.Length; i++) {
+    threesArr[i] = i*3;
+}
+
+Console.WriteLine(threesArr[0]);
+Console.WriteLine(threesArr[threesArr.Length-1]);
