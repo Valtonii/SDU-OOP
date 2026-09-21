298eae258cfb44e2ba4ea54e7eb51c5bcc2c9d0d
Valton Morina <Valton.morina301@hotmail.com>
2026-09-21 23:42:47 +0200

    07-07 exercise done

---
+++ kap7/07-07-Sudoku-Puzzle/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+
+The inner arrays are their own seperate space somewhere in the memory,
+where the outer arrays have access to them by storing references to them.
+these references would basically point at where the arrays themself are,
+in other words, where the rows are in the memory. in an marray, its all
+one block and one array, one calculation and one jump
+
+*/
