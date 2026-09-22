e2cb3294e60fa4412555806a225ac4c1a0ae17dc
Valton Morina <Valton.morina301@hotmail.com>
2026-09-22 12:42:52 +0200

    07-15 exercise done

---
+++ kap7/07-15-Person-Struct/Program.cs
@@ -0,0 +1,11 @@
+﻿Person p = new Person {name="Valton", age=19};
+
+Console.WriteLine();
+Console.WriteLine("Name: "+p.name);
+Console.WriteLine();
+Console.WriteLine("Age: "+p.age);
+
+class Person {
+    public string name;
+    public int age;
+}
